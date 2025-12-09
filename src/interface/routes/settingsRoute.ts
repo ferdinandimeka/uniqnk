f6e89c7e65721dcc80f21596e597beda4938c819
ferdinandimeka <ferdinandimeka20@gmail.com>
2025-12-09 16:13:20 +0100

    settings

--- src/interface/routes/settingsRoute.ts
+++ src/interface/routes/settingsRoute.ts
@@ -53,31 +53,135 @@ const controller = new SettingsController(
  *   description: User settings endpoints
  */
 
-// Get settings
+
+/**
+ * @swagger
+ * /api/v1/settings/{userId}:
+ *   get:
+ *     summary: Get all settings for a user
+ *     tags: [Settings]
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: User settings fetched successfully
+ */
 router.get("/:userId", (req, res, next) => controller.getSettings(req, res, next));
 
-// Profile settings
-router.put("/:userId/profile", (req, res, next) => controller.updateProfileSettings(req, res, next));
+/**
+ * @swagger
+ * /api/v1/settings/{userId}/preferences:
+ *   put:
+ *     summary: Update account preferences (theme, language, data usage)
+ *     tags: [Settings]
+ */
+router.put("/:userId/preferences", (req, res, next) => controller.updateProfileSettings(req, res, next));
 
-// Notification settings
+/**
+ * @swagger
+ * /api/v1/settings/{userId}/notifications:
+ *   put:
+ *     summary: Update notification settings
+ *     tags: [Settings]
+ */
 router.put("/:userId/notifications", (req, res, next) => controller.updateNotificationSettings(req, res, next));
 
-// Privacy settings
+/**
+ * @swagger
+ * /api/v1/settings/{userId}/privacy:
+ *   patch:
+ *     summary: Update privacy settings
+ *     tags: [Settings]
+ */
 router.put("/:userId/privacy", (req, res, next) => controller.updatePrivacySettings(req, res, next));
 
 // Account security
 router.put("/:userId/security", (req, res, next) => controller.updateSecuritySettings(req, res, next));
 
-// Restrictions
+/**
+ * @swagger
+ * /api/v1/settings/{userId}/restrictions:
+ *   patch:
+ *     summary: Update restriction settings (blocked accounts, limits)
+ *     tags: [Settings]
+ */
 router.put("/:userId/restrictions", (req, res, next) => controller.updateRestrictions(req, res, next));
 
-// Block user
-router.post("/:userId/block", (req, res, next) => controller.blockUserHandler(req, res, next));
+/**
+ * @swagger
+ * /api/v1/settings/{userId}/block:
+ *   post:
+ *     summary: Block another user
+ *     tags: [Settings]
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *         description: ID of the user performing the block
+ *       - in: body
+ *         name: data
+ *         required: true
+ *         description: User to block
+ *         schema:
+ *           type: object
+ *           properties:
+ *             targetUserId:
+ *               type: string
+ *               example: "678ffe2ba09d3a21e3bf45b9"
+ *     responses:
+ *       200:
+ *         description: User blocked successfully
+ *       400:
+ *         description: User already blocked or invalid target
+ *       404:
+ *         description: User not found
+ */
+router.post("/:userId/block", (req, res, next) =>
+    controller.blockUserHandler(req, res, next)
+);
 
-// Unblock user
-router.post("/:userId/unblock", (req, res, next) => controller.unblockUserHandler(req, res, next));
+/**
+ * @swagger
+ * /api/v1/settings/{userId}/unblock:
+ *   post:
+ *     summary: Unblock a user
+ *     tags: [Settings]
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *         description: ID of the user performing the unblock
+ *       - in: body
+ *         name: data
+ *         required: true
+ *         description: User to unblock
+ *         schema:
+ *           type: object
+ *           properties:
+ *             targetUserId:
+ *               type: string
+ *               example: "678ffe2ba09d3a21e3bf45b9"
+ *     responses:
+ *       200:
+ *         description: User unblocked successfully
+ *       400:
+ *         description: User is not blocked
+ *       404:
+ *         description: User not found
+ */
+router.post("/:userId/unblock", (req, res, next) =>
+    controller.unblockUserHandler(req, res, next)
+);
 
-// Report a problem
+/**
+ * @swagger
+ * /api/v1/settings/{userId}/report:
+ *   post:
+ *     summary: Report a problem with screenshot + description
+ *     tags: [Settings]
+ */
 router.post("/:userId/report", (req, res, next) => controller.reportProblem(req, res, next));
 
 // Support
